80ad13029909deea6e30e2d8fb7f14ee61105dd2
Arthur Sarry <arthur.sarry@gmail.com>
2022-06-12 17:39:50 +0200

    rendering/light: Initial commit

---
+++ rendering/light.py
@@ -0,0 +1,23 @@
+class Light:
+    def __init__(self, color):
+        self.color = color
+
+
+class AmbientLight(Light):
+    def __init__(self, color):
+        super().__init__(color)
+
+
+class DirectionalLight(Light):
+    def __init__(self, color, direction, intensity):
+        super().__init__(color)
+        self.direction = direction
+        self.intensity = intensity
+
+
+class PointLight(Light):
+    def __init__(self, color, position, intensity, atten_factors=(1.0, 0.1, 0.01)):
+        super().__init__(color)
+        self.position = position
+        self.intensity = intensity
+        self.atten_factors = atten_factors
