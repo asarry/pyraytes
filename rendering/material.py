0d87bc21e4562378f2be0bb64ed0fb2c59a4d3ba
Arthur Sarry <arthur.sarry@gmail.com>
2022-06-12 19:08:28 +0200

    rendering/material: Updating the CheckeredMaterial in order to simplify objects rendering

--- rendering/material.py
+++ rendering/material.py
@@ -1,4 +1,5 @@
 from rendering.color import Color
+import math
 
 
 class Material:
@@ -24,14 +25,19 @@ class CheckeredMaterial(Material):
         specular_rate,
         specular_exponent,
         reflection_rate,
+        cell_size,
     ):
         super().__init__(
             prim_color, diffuse_rate, specular_rate, specular_exponent, reflection_rate
         )
         self.sec_color = sec_color
+        self.cell_size = cell_size
 
     def color_at(self, position):
-        if True:
+        if (
+            math.floor(position.x / self.cell_size)
+            + math.floor(position.z / self.cell_size)
+        ) % 2 == 0:
             return self.color
         else:
-            return sec_color
+            return self.sec_color
