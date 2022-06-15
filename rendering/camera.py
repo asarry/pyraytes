c54e1d409fa80d880c757288c1cd168c03bc97a5
Arthur Sarry <arthur.sarry@gmail.com>
2022-06-15 20:20:13 +0200

    rendering/camera: Simplifying the code

--- rendering/camera.py
+++ rendering/camera.py
@@ -4,38 +4,27 @@ import math
 
 
 class Camera:
-    def __init__(
-        self, position, direction, up, aspect_ratio, field_of_view=math.pi / 4
-    ):
+    def __init__(self, position, direction, up, aspect_ratio, fov=math.pi / 4):
         self.position = position
         self.direction = direction
         self.up = up
         self.aspect_ratio = aspect_ratio
-        self.field_of_view = field_of_view
+        self.fov = fov
 
-        self._height = math.tan(self.field_of_view / 2) * 2
+        self._height = math.tan(self.fov / 2) * 2
         self._width = self.aspect_ratio * self._height
 
-        self._rv = Vector3D.cross(self.up, self.direction).normalize()
-        self._uv = Vector3D.cross(self.direction, self._rv).normalize()
+        self._right = Vector3D.cross(self.up, self.direction).normalize()
+        self._up = Vector3D.cross(self.direction, self._right).normalize()
 
-        self._ld = (
-            self.position
-            + self.direction
-            + -self._rv * self._width / 2
-            + -self._uv * self._height / 2
-        )
-
-    def corresponding_ray(self, frame_size, pixel_index):
+    def corresponding_ray(self, image_size, pixel_index):
         pixel_pos = (
-            self._ld
-            + self._rv * (pixel_index[0] / frame_size[0] * self._width)
-            + self._uv * (pixel_index[1] / frame_size[1] * self._height)
+            self.direction
+            + self._right * self._width * (pixel_index[0] / image_size[0] - 0.5)
+            + self._up * self._height * (pixel_index[1] / image_size[1] - 0.5)
         )
 
-        return Ray(self.position, (pixel_pos - self.position).normalize())
+        return Ray(self.position, (pixel_pos).normalize())
 
-    def create_lookat(position, target, up, aspect_ratio, field_of_view=math.pi / 4):
-        return Camera(
-            position, (target - position).normalize(), up, aspect_ratio, field_of_view
-        )
+    def create_lookat(position, target, up, aspect_ratio, fov=math.pi / 4):
+        return Camera(position, (target - position).normalize(), up, aspect_ratio, fov)
